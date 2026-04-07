d56957743b92a38742a049a3e45296c0d7639c2d
cchun21 <dudeitscarter@gmail.com>
2026-04-07 19:21:46 -0400

    registration fixed and operational

--- registration.php
+++ registration.php
@@ -12,6 +12,8 @@
 <input type="password" id="pass" name="psw" value=""><br>
 <label for="psw2">Reenter Password:</label>
 <input type="password" id="pass" name="psw2" value=""><br>
+<label for="email">Email:</label>
+<input type="email" id="email" name="email" value=""><br>
 <br>
 <input type="submit" name="login" value="Submit">
 </form>
@@ -19,17 +21,19 @@
 </html>
 
 <?php
+session_start();
 //attach the secure data file
 include('/home/cc1814/P/dbh.inc');
 $conn = mysqli_connect($db_server,$user,$password,$db_names);//Method -1
 //Get the Data
 $username=trim(addslashes($_POST["uname"]));
 $passcode=trim(addslashes($_POST["psw"]));
+$email=trim(addslashes($_POST["email"]));
 
 if(isset($_POST["login"])){
 if(!empty($_POST["uname"])&& !empty($_POST["psw"])&& !empty($_POST["psw2"])){
 if($_POST["psw"]==$_POST["psw2"]){
-$sql_string="INSERT INTO tempo(Username,pass) VALUES ('$username','$passcode')";
+$sql_string="INSERT INTO LoginSystem(username,password,email) VALUES ('$username','$passcode','$email')";
 mysqli_query($conn,$sql_string);
 echo "User data is registered";
 }
@@ -46,4 +50,5 @@ else{
 echo "waiting for entry";
 }
 mysqli_close($conn);
+session_destroy();
 ?>
