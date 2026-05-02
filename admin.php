8416872867b81819894726b927f13373a8bf6e90
Hunter H. <hunter-harris1@hotmail.com>
2026-05-02 15:39:06 -0400

    Gave admin ability to view all transactions

--- admin.php
+++ admin.php
@@ -19,6 +19,16 @@
     // Link vars file and connect to DB.
     include('/home/hah1049/PHP-Includes/php-vars.inc');
     $conn = mysqli_connect($db_server, $user, $password, $db_names);
+
+    $sql = "SELECT trans_id, user_id, total, `date-time` FROM transactions ORDER BY `date-time` ASC";
+    $order_query = $conn->prepare($sql);
+
+    if ($order_query === false) {
+        die("SQL Error: " . $conn->error);
+    }
+
+    $order_query->execute();
+    $order_result = $order_query->get_result();
 ?>
 
 <!DOCTYPE html>
@@ -71,7 +81,39 @@
 
         <section class="hero" style="background-image: url('https://images.unsplash.com/photo-1414235077428-338989a2e8c0?auto=format&fit=crop&w=1600&q=80');">
             <div class="container overlay">
-
-
+                <div class="row">
+                    <div class="col-12">
+                        <h2 class="text-warning border-bottom pb-2">Transaction List</h2>
+                        
+                        <?php if ($order_result->num_rows > 0): ?>
+                            <div class="table-responsive mt-4">
+                                <table class="table table-dark table-hover table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>Order ID</th>
+                                            <th>User ID</th>
+                                            <th>Total Spent</th>
+                                            <th>Date & Time</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php while ($row = $order_result->fetch_assoc()): ?>
+                                            <tr>
+                                                <td>#<?php echo $row['trans_id']; ?></td>
+                                                <td>#<?php echo $row['user_id']; ?></td>
+                                                <td>$<?php echo number_format($row['total'], 2); ?></td>
+                                                <td><?php echo date('M d, Y - h:i A', strtotime($row['date-time'])); ?></td>
+                                            </tr>
+                                        <?php endwhile; ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        <?php else: ?>
+                            <div class="alert alert-info mt-4">
+                                You haven't placed any orders yet!
+                            </div>
+                        <?php endif; ?>
+                    </div>
+                </div>
             </div>
         </section>
